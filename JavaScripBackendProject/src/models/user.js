c655932236e1f3d21c8a3b7c77fda734dbe0aed8
Dipongkar Barmon <dipongkorbarman02@gmail.com>
2025-11-29 17:43:21 +0600

    setup model and learn about jwt,bcrypt,mongoose-aggregate-paginate-v2,mongoose pre,plagin method, setup generateAccessToken ,generateRefreshToken

---
+++ JavaScripBackendProject/src/models/user.js
@@ -0,0 +1,96 @@
+ 
+import mongoose,{Schema} from "mongoose";
+import bcrypt from "bcrypt"
+import JWT from 'jsonwebtoken'
+import { useState } from "react";
+
+
+const userSchema=new Schema({
+    fullname:{
+        type:String,
+        required:true,
+        lowecase:true,
+        trim:true,
+        index:true
+    },
+    username:{
+        type:String,
+        required:true,
+        unique:true,
+        lowecase:true,
+        trim:true,
+        index:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        lowecase:true,
+        trim:true,
+        index:true
+    },
+    avater:{
+      type:String,//cloudUrl
+        required:true,
+    },
+    coverImage:{
+      type:String,//cloudUrl
+      
+    },
+    watchHistry:[
+      {
+        type:Schema.Types.ObjectId,
+        ref:'video'
+      }
+    ],
+    password:{
+      type:String,
+      required:[true,"Password is required"]
+    },
+    refreshToken:{
+        type:String
+    }
+},{timestamps:true});
+
+userSchema.pre('save',async function (next) {
+     const user=this;
+     if(!user.isModified('password')){
+      return next();
+     }
+     const hassPasword=bcrypt.hash(user.password,10);
+     this.password=hassPasword;
+     next()
+});
+
+userSchema.methods.isPasswordCorrect=async function(password) {
+    return await bcrypt.compare(password,this.password)
+}
+
+userSchema.methods.generateAccessToken=function(){
+   return JWT.sign(
+    {
+      _id:this._id,
+      fullname:this.fullname,
+      username:this.username,
+      email:this.email
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+    }
+   )
+}
+userSchema.methods.generateRefreshToken=function(){
+  return JWT.sign(
+    {
+      _id:this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+    }
+   )
+}
+
+export const User=mongoose.model('user',userSchema);
+
